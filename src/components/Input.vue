13483ac3b311ec782d4a62e1428a18d511ee512e
VaynePeng <vaynepeng@gmail.com>
2024-02-06 14:53:47 +0800

    feat: add input component

---
+++ src/components/Input.vue
@@ -0,0 +1,30 @@
+<template>
+  <input
+    class="rounded-md focus:ring-2 focus:ring-black bg-background flex-1 p-2"
+    :value="value"
+    :placeholder="placeholder"
+    @input="onInput"
+  />
+</template>
+
+<script setup lang="ts">
+import { defineEmits } from 'vue'
+
+interface Props {
+  value: string | number;
+  placeholder: string
+}
+withDefaults(defineProps<Props>(), {
+  value: '',
+  placeholder: ''
+})
+
+const emits = defineEmits<{
+  onInput: [value: string ]
+}>()
+
+const onInput = (e: Event) => {
+  const target = e.target as HTMLInputElement
+  emits('onInput', target.value)
+}
+</script>
