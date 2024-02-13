2bda94d95fad7c976a6f5cc7b856bfb5115045ea
VaynePeng <vaynepeng@gmail.com>
2024-02-13 19:41:27 +0800

    feat: add menu component

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,15 +1,73 @@
 <template>
-  <div class="p-2">
-    <div class="hover:bg-gray-100/90 rounded p-2 cursor-pointer select-none flex items-center text-sm">
-      <Icon name="icon-home" class="mr-2 flex-none" />
-      <span class="flex-1 text-nowrap overflow-hidden text-ellipsis">
-        首页
-      </span>
-      <Icon name="icon-down" class="ml-2 flex-none" :class="{'rotate-180': false}" />
+  <div ref="menuRef" class="px-2">
+    <div v-for="element in menuList" :key="element.id">
+      <div class="hover:bg-gray-100/90 rounded p-2 cursor-pointer select-none flex items-center text-sm" @click="openMenu(element.id)">
+        <Icon v-if="element.icon" :name="element.icon" class="mr-2 flex-none" />
+        <span class="flex-1 text-nowrap overflow-hidden text-ellipsis">
+          {{ element.title }}
+        </span>
+        <Icon
+          v-if="element.children?.length"
+          name="icon-down"
+          class="ml-2 flex-none"
+          :class="{'rotate-180': isActive(element.id)}"
+        />
+      </div>
+      <Menu
+        v-if="element.children"
+        :ref="(el) => menuChildrenRef[element.id] = el"
+        :menu-list="element.children"
+        class="h-0 overflow-hidden transition-all ease-linear duration-300"
+      />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref, withDefaults } from 'vue'
 import Icon from './Icon.vue'
+
+interface Menu {
+  id: number
+  title: string
+  path?: string
+  icon?: string
+  children?: Array<Menu>
+}
+interface Props {
+  menuList: Array<Menu>
+}
+withDefaults(defineProps<Props>(), {
+  menuList: () => []
+})
+
+const activeMenuList = ref<Array<number>>([])
+
+const menuRef = ref<HTMLElement | null>(null)
+const menuChildrenRef = ref<Record<number, any>>({})
+
+defineExpose({
+  node: menuRef
+})
+
+const openMenu = (id: number) => {
+  const index = activeMenuList.value.indexOf(id)
+  const node = menuChildrenRef.value[id]?.node
+  if (index === -1) {
+    if (node) {
+      node.style.height = `${node.scrollHeight}px`
+    }
+    activeMenuList.value.push(id)
+  }
+  else {
+    if (node) {
+      node.style.height = '0px'
+    }
+    activeMenuList.value.splice(index, 1)
+  }
+}
+
+const isActive = (id: number) => {
+  return activeMenuList.value.includes(id)
+}
 </script>
--- src/components/Sider.vue
+++ src/components/Sider.vue
@@ -1,12 +1,12 @@
 <template>
   <aside
-    class="w-[200px] fixed top-[60px] bottom-0 transition-all ease-linear delay-300 shadow bg-white"
+    class="w-[200px] fixed top-[60px] bottom-0 transition-all ease-linear shadow bg-white py-2"
     :class="{ '-translate-x-[200px]': !isHeaderVisible }"
   >
-    <Menu />
+    <Menu :menu-list="menuList" />
   </aside>
   <div
-    class="w-[200px] transition-all ease-linear delay-300 max-md:hidden"
+    class="w-[200px] transition-all ease-linear max-md:hidden"
     :class="{ 'w-0': !isHeaderVisible }"
   />
 </template>
@@ -17,4 +17,53 @@ import { inject } from 'vue'
 import Menu from './Menu.vue'
 
 const isHeaderVisible = inject('isHeaderVisible')
+
+const menuList = [
+  {
+    id: 1,
+    title: '首页',
+    icon: 'icon-home',
+    path: '/',
+    children: [
+      {
+        id: 11,
+        title: '首页',
+        path: '/'
+      },
+      {
+        id: 12,
+        title: '文章',
+        path: '/article'
+      },
+      {
+        id: 13,
+        title: '标签',
+        path: '/tag'
+      },
+      {
+        id: 14,
+        title: '分类',
+        path: '/category'
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: '文章',
+    icon: 'icon-menu',
+    path: '/article'
+  },
+  {
+    id: 3,
+    title: '标签',
+    icon: 'icon-home',
+    path: '/tag'
+  },
+  {
+    id: 4,
+    title: '分类',
+    icon: 'icon-menu',
+    path: '/category'
+  }
+]
 </script>
